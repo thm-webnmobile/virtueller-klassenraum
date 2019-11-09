3f5a6e8e4179c50683d91aaf2c8cc3824b5711a2
David Bauer <david.bauer@iem.thm.de>
2019-11-09 09:53:12 +0100

    init submit

--- chat-app/src/components/Chat/MessageBox/MessageBox.js
+++ chat-app/src/components/Chat/MessageBox/MessageBox.js
@@ -8,27 +8,26 @@ const MessageBox = ({ onSendMessage: pushSendMessage }) => {
     const [message, setMessage] = useState("");
 
     return (
-            <Row>
-                <Col sm={10}>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="Message" 
-                        onChange={evt => setMessage(evt.target.value)}
-                        onKeyDown={evt => {
-                            if (evt.key === "Enter") {
-                                evt.preventDefault();
-                                pushSendMessage(message);
-                                setMessage("");
-                            }
-                        }}
-                        value={message}
-                    />
-                </Col>
-                <Col sm={2}>
-                    <Button variant="primary" onClick={evt => pushSendMessage(message)}>Send Message</Button>
-                </Col>
-            </Row>
-
+        <Row>
+            <Col sm={10}>
+                <Form.Control 
+                    type="text" 
+                    placeholder="Message" 
+                    onChange={evt => setMessage(evt.target.value)}
+                    onKeyDown={evt => {
+                        if (evt.key === "Enter") {
+                            evt.preventDefault();
+                            pushSendMessage(message);
+                            setMessage("");
+                        }
+                    }}
+                    value={message}
+                />
+            </Col>
+            <Col sm={2}>
+                <Button variant="primary" onClick={evt => pushSendMessage(message)}>Send Message</Button>
+            </Col>
+        </Row>
     );
 };
 
