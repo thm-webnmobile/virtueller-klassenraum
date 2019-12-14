8892e66cfe867803931fa86cc346d2b3754dc898
David Bauer <david.bauer@iem.thm.de>
2019-12-14 16:51:44 +0100

    syntax

--- services/src/Database.js
+++ services/src/Database.js
@@ -5,31 +5,31 @@ const dbName = 'myproject';
 
 var state = {
     db: null,
-}
+};
 
 // Connect to the database
 exports.connect = function(url, done) {
-    if (state.db) return done()
+    if (state.db) return done();
 
     MongoClient.connect(url, function(err, client) {
-        if (err) return done(err)
+        if (err) return done(err);
         state.db = client.db(dbName);
-        done()
-    })
-}
+        done();
+    });
+};
 
 // Gets the database
 exports.get = function() {
-    return state.db
-}
+    return state.db;
+};
 
 // Closes connection
 exports.close = function(done) {
     if (state.db) {
-            state.db.close(function(err, result) {
-            state.db = null
-            state.mode = null
-            done(err)
-            })
-    }
+        state.db.close(function(err, result) {
+            state.db = null;
+            state.mode = null;
+            done(err);
+        });
+    };
 }
